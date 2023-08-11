e256d435b43b3823b564ba01cc1cb0b85f20ca75
yayayagi721 <snoopy1995.1.25@gmail.com>
2023-08-12 03:06:51 +0900

    fix for release

--- src/components/pages/mainPage/MainPage.tsx
+++ src/components/pages/mainPage/MainPage.tsx
@@ -1,9 +1,21 @@
 import MoonCanvas from "../../features/moonCanvas/MoonCanvas";
 import Overlays from "../../features/overlays/Overlays";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 
 function MainPage() {
+  useEffect(() => {
+    // Google Analytics 測定 ID を入力して設定
+    ReactGA.initialize("G-6GHMVE8VC7");
+    ReactGA.send({
+      hitType: "pageview",
+      // アクセスしたパス (pathname) とクエリ文字列 (search) を送付する (必要に応じて編集する)
+      page: "top",
+    });
+  }, []);
+
   return (
-    <div>
+    <div className="main-page">
       <MoonCanvas />
       <Overlays />
     </div>
